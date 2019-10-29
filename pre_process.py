1a5a318a8cafe9fee3e848ad762b906de4fe2c8c
Tanmay Aggarwal <tanmay.agg@gmail.com>
2019-10-28 23:22:38 -0700

    Function to pre-process data

---
+++ pre_process.py
@@ -0,0 +1,14 @@
+# helps pre-process the text to make the training easier
+
+# converts punctuations to tokens (e.g., period is changed to <PERIOD>)
+# removes all words that show up five or fewer times in the dataset to reduce issues due to noise in the data
+# returns a list of words in the text
+
+import utils
+
+def pre_process(text):
+    words = utils.preprocess(text)
+
+    print(words[:30])
+
+    return words
