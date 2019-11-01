7a714c75f4f26a1cc01938088a73a3edb379cc25
Tanmay Aggarwal <tanmay.agg@gmail.com>
2019-10-31 20:11:18 -0700

    Updated file to make and generate batches

--- get_batches.py
+++ get_batches.py
@@ -14,16 +14,21 @@ def get_batches(words, batch_size, window_size=5):
     # returns batches of input and target data using the get_target function
 
     n_batches = len(words)//batch_size
-
+    #print(n_batches)
     # only full batches
     words = words[:n_batches*batch_size]
+    #print(len(words))
 
     for idx in range(0, len(words), batch_size):
         x, y = [], []
+        #print(idx)
         batch = words[idx:idx+batch_size]
+        #print(len(batch))
         for ii in range(len(batch)):
             batch_x = batch[ii]
+            #print(batch_x)
             batch_y = get_target(batch, ii, window_size)
             y.extend(batch_y)
-            y.extend([batch_x]*len(batch_y))
+            x.extend([batch_x]*len(batch_y))
         yield x, y
+    return x, y
